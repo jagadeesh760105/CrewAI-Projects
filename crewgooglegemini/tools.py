9974c3dd59babddfecf11aa683043360103a179a
jagadeesh760105 <jagadeeshnaikpalthya@gmail.com>
2025-04-14 10:01:06 +0530

    updated

--- crewgooglegemini/tools.py
+++ crewgooglegemini/tools.py
@@ -4,8 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-os.environ['SERPER_API_KEY'] = os.getenv('SERPER_API_KEY')
+# # os.environ['SERPER_API_KEY'] = os.getenv('SERPER_API_KEY')
 
+# serper_api_key = os.getenv('SERP_API_KEY')
+# if serper_api_key is None:
+#     raise ValueError("SERP_API_KEY is not set in the environment!")
 
 from crewai_tools import SerperDevTool
 
